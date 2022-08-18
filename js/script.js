acd2f407cda4dce09e807f176764f3657b0a4f2f
Michael Kelly <m@michaelkelly.org>
2022-08-18 18:51:23 -0400

    Remove password strength checking

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 'use strict';
+var selectField = document.getElementById('passphrase_select');
+var passwordField = document.getElementById('passphrase');
+var button = document.querySelector('.btn-generate');
 
 // Cryptographic replacement for Math.random()
 function randomNumberBetweenZeroAndOne() {
@@ -28,63 +31,17 @@ function setStyleFromWordNumber(passwordField, numberOfWords) {
   passwordField.setAttribute('style', 'font-size: ' + newSize + 'px;');
 }
 
-function convertSecondsToReadable(seconds) {
-  var timeString = '';
-  var crackabilityColor = 'green';
-
-  // Enumerate all the numbers
-  var numMilliseconds = seconds * 1000;
-  var numSeconds     = Math.floor(seconds);
-  var numMinutes     = Math.floor(numSeconds / 60);
-  var numHours       = Math.floor(numSeconds / (60 * 60));
-  var numDays        = Math.floor(numSeconds / (60 * 60 * 24));
-  var numYears       = Math.floor(numSeconds / (60 * 60 * 24 * 365));
-  var numCenturies   = Math.floor(numSeconds / (60 * 60 * 24 * 365 * 100));
-
-  if (numMilliseconds < 1000) {
-    timeString = numMilliseconds + ' milliseconds';
-  } else if (numSeconds < 60) {
-    timeString = numSeconds + ' seconds';
-  } else if (numMinutes < 60) {
-    timeString = numMinutes + ' minutes';
-  } else if (numHours < 24) {
-    timeString = numHours + ' hours';
-  } else if (numDays < 365) {
-    timeString = numDays + ' days';
-  } else if (numYears < 100) {
-    timeString = numYears + ' years';
-  } else {
-    timeString = numCenturies + ' centuries';
-  }
-
-  return timeString.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+function showPassphrase(value) {
+  passwordField.innerHTML = value;
 }
 
-function calculateAndSetCrackTime() {
-  var timeToCrack = zxcvbn(passwordField.value);
-  var readableCrackTime = convertSecondsToReadable(timeToCrack.crack_time);
-  document.querySelector('.crack-time').innerHTML = readableCrackTime;
+function generateNewPassphrase() {
+  var numberOfWords = selectField.options[selectField.selectedIndex].value;
+  showPassphrase(generatePassword(numberOfWords));
 }
-
-var selectField = document.getElementById('passphrase_select');
-var passwordField = document.getElementById('passphrase');
-var button = document.querySelector('.btn-generate');
 
 // Initially run it upon load
-var defaultNumberOfWords = parseInt(document.getElementById('passphrase_select').value);
-passwordField.setAttribute('value', generatePassword(defaultNumberOfWords));
-setStyleFromWordNumber(passwordField, defaultNumberOfWords);
-calculateAndSetCrackTime();
+generateNewPassphrase();
 
 // Listen for a button click
-button.addEventListener('click', function() {
-  var numberOfWords = selectField.options[selectField.selectedIndex].value;
-  passwordField.value = generatePassword(numberOfWords);
-  setStyleFromWordNumber(passwordField, numberOfWords);
-  calculateAndSetCrackTime();
-});
-
-// Listen for password value change
-passwordField.addEventListener('input', function (evt) {
-  calculateAndSetCrackTime();
-});
+button.addEventListener('click', generateNewPassphrase);
